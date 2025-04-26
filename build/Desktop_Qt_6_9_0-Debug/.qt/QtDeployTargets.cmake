2f708db49e166047cf0f5d79827663571ef85ff8
Qube Sat <qubesat@qubesat.lan>
2025-04-26 05:01:35 -0400

    Removed build dir

--- build/Desktop_Qt_6_9_0-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_qt6-sensors-bmp280-i2c_FILE /home/qubesat/Projects/qt-sensors-bmp280-i2c/build/Desktop_Qt_6_9_0-Debug/libqt6-sensors-bmp280-i2c.so)
-set(__QT_DEPLOY_TARGET_qt6-sensors-bmp280-i2c_TYPE SHARED_LIBRARY)
-set(__QT_DEPLOY_TARGET_qt6-sensors-bmp280-i2c-plugin_FILE /home/qubesat/Projects/qt-sensors-bmp280-i2c/build/Desktop_Qt_6_9_0-Debug/libqt6-sensors-bmp280-i2c-plugin.so)
-set(__QT_DEPLOY_TARGET_qt6-sensors-bmp280-i2c-plugin_TYPE SHARED_LIBRARY)
